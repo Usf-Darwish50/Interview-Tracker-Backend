5c5851ca30968dea135b35c84df1be0a9bf238dd
Usf-Darwish50 <youssefdarwish50@yahoo.com>
2025-08-15 03:26:55 +0300

    fixes for Railway compatibility

--- src/main/java/com/example/Interview_Tracker/InterviewTrackerApplication.java
+++ src/main/java/com/example/Interview_Tracker/InterviewTrackerApplication.java
@@ -1,11 +1,14 @@
 package com.example.Interview_Tracker;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InterviewTrackerApplication {
 
 	public static void main(String[] args) {
+		SpringApplication.run(InterviewTrackerApplication.class, args);
+
 		//EnvironmentLoader.load(args);
 
 
