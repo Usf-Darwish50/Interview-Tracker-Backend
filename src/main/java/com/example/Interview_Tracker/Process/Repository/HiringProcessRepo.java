d1c395da7f6cb11d5b9bd9a0a140254ad3a4e601
Masr franca <ahmedmohamedfathy18@gmail.com>
2025-08-17 21:57:38 +0300

    iam do process {entity and repo and service and controller}

---
+++ src/main/java/com/example/Interview_Tracker/Process/Repository/HiringProcessRepo.java
@@ -0,0 +1,10 @@
+package com.example.Interview_Tracker.Process.Repository;
+
+
+import com.example.Interview_Tracker.Process.Model.HiringProcess;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HiringProcessRepo extends JpaRepository<HiringProcess, Integer> {
+}
+
+
